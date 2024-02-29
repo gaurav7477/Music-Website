2aa4543162d2a272a901c6f8d5ce5a41a43c1c94
gaurav7477 <bairagi7477@gmail.com>
2024-03-01 00:16:44 +0530

    Navbar and Hero section components done

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import HeroSection from '@/components/HeroSection'
+import React from 'react'
+
+function page() {
+  return (
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-while/[0.02]">
+      <HeroSection/>
+    </main>
+  )
+}
+
+export default page
