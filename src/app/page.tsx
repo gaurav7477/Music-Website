68a251876ff77fae0e12d11b42ee784cc7da7b24
gaurav7477 <bairagi7477@gmail.com>
2024-03-01 08:31:03 +0530

    FeaturedCources Completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Featuredcourses from '@/components/Featuredcourses'
 import HeroSection from '@/components/HeroSection'
 import React from 'react'
 
@@ -5,6 +6,7 @@ function page() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-while/[0.02]">
       <HeroSection/>
+      <Featuredcourses/>
     </main>
   )
 }
