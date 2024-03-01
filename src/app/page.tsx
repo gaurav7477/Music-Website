24872cde5d6811bc91e2ea0397bd85afd425346d
gaurav7477 <bairagi7477@gmail.com>
2024-03-01 22:36:19 +0530

    whychosseus section Completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Featuredcourses from '@/components/Featuredcourses'
 import HeroSection from '@/components/HeroSection'
+import WhyChooseUs from '@/components/WhyChooseUs'
 import React from 'react'
 
 function page() {
@@ -7,6 +8,7 @@ function page() {
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-while/[0.02]">
       <HeroSection/>
       <Featuredcourses/>
+      <WhyChooseUs />
     </main>
   )
 }
